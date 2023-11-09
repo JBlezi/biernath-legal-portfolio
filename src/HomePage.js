fa9639c2042e8271cc89da0d6f147900163738e1
Julius Blezinger <jgoldappel@gmail.com>
2023-11-09 21:26:51 +0100

    makes page responsive on all screen sizes

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,26 +8,36 @@ const HomePage = () => {
   const { t } = useTranslation('');
 
   return (
-    <div className="mx-8">
-      <div className="h-screen relative flex">
+    <div className="mx-8 md:mx-16">
+      <div className="h-screen md:h-[70vh] relative flex">
         <div className='my-auto'>
-          <h1 className="text-4xl font-bold mb-2">Biernath Legal</h1>
-          <h2 className='text-2xl font-semibold mb-8'>{t('hero.subheader')}</h2>
-          <p className='text-xl font-medium'>{t('hero.description')}</p>
+          <h1 className="text-4xl md:text-6xl font-bold mb-2">Biernath Legal</h1>
+          <h2 className='text-2xl md:text-3xl font-semibold mb-8'>{t('hero.subheader')}</h2>
+          <p className='text-xl md:text-2xl font-medium'>{t('hero.description')}</p>
         </div>
-        <FaChevronDown className='text-2xl absolute bottom-24 left-1/2 left-1/2 transform -translate-x-1/2'/>
+        <FaChevronDown className='md:hidden text-2xl md:text-4xl absolute bottom-24 left-1/2 left-1/2 transform -translate-x-1/2'/>
       </div>
-      <Expertise icon={<FaCloudsmith/>} header={t('expertise.it-law.header')} content={t('expertise.it-law.content')}/>
-      <Expertise icon={<FaServer/>} header={t('expertise.data-law.header')} content={t('expertise.data-law.content')}/>
-      <Expertise icon={<FaPlay/>} header={t('expertise.media-law.header')} content={t('expertise.media-law.content')}/>
-      <Expertise icon={<FaShoppingCart/>} header={t('expertise.ecommerce-law.header')} content={t('expertise.ecommerce-law.content')}/>
-      <div className='my-24'>
-        <h2 className='text-2xl font-semibold underline mb-8'>Benedikt Biernath, {t('bio.header')}</h2>
-        <p className='text-xl font-medium'>{t('bio.description')}</p>
+      <div>
+        <div className='lg:flex lg:flex-row'>
+          <div className="lg:pr-8">
+            <Expertise icon={<FaCloudsmith/>} header={t('expertise.it-law.header')} content={t('expertise.it-law.content')}/>
+          </div>
+          <Expertise icon={<FaServer/>} header={t('expertise.data-law.header')} content={t('expertise.data-law.content')}/>
+        </div>
+        <div className='lg:flex lg:flex-row'>
+          <div className="lg:pr-8">
+            <Expertise icon={<FaPlay/>} header={t('expertise.media-law.header')} content={t('expertise.media-law.content')}/>
+          </div>
+          <Expertise icon={<FaShoppingCart/>} header={t('expertise.ecommerce-law.header')} content={t('expertise.ecommerce-law.content')}/>
+        </div>
+      </div>
+      <div className='my-24 md:my-48'>
+        <h2 className='text-2xl md:text-4xl font-semibold underline mb-8'>Benedikt Biernath, {t('bio.header')}</h2>
+        <p className='text-xl md:text-2xl font-medium'>{t('bio.description')}</p>
       </div>
       <div class='wrap my-16'>
         <div class='fill-wrap'>
-          <a href='mailto:ra@biernath.com' class='btn btn-desert btn-b-t text-2xl lg:text-4xl p-4 md:p-6 rounded-full'>{t('navbar.contact')}</a>
+          <a href='mailto:ra@biernath.com' class='btn btn-desert btn-b-t text-2xl md:text-4xl p-4 md:p-6 rounded-full'>{t('navbar.contact')}</a>
         </div>
       </div>
     </div>
--- src/expertise.js
+++ src/expertise.js
@@ -6,12 +6,12 @@ const Expertise = (props) => {
   const icon = props.icon;
 
   return (
-    <div className="my-12">
+    <div className="my-12 lg:mb-16 lg:mt-0">
       <div className='flex flex-row'>
         <div className='text-4xl mr-4'>{icon}</div>
-        <h2 className='text-2xl font-semibold mb-4 underline'>{header}</h2>
+        <h2 className='text-2xl md:text-4xl font-semibold mb-4 underline'>{header}</h2>
       </div>
-      <p className='text-xl font-medium'>{content}</p>
+      <p className='text-xl md:text-2xl font-medium'>{content}</p>
     </div>
   );
 };
