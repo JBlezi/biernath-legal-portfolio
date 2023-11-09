b0c68e81513b04bc6e24b161443a59ee01da0d96
Julius Blezinger <jgoldappel@gmail.com>
2023-11-09 11:33:46 +0100

    adds font, hero text and chevron

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold text-center my-8">Welcome to My Portfolio</h1>
+    <div className="mx-8 h-screen relative">
+      <h1 className="text-4xl font-bold pt-64 mb-2">Biernath Legal</h1>
+      <h2 className='text-2xl font-semibold mb-8'>Spezialisierte Rechtsberatung</h2>
+      <p className='text-xl font-medium'>Ich berate in allen Fragen zum IT-Recht, Datenschutzrecht, Medienrecht und E-Commerce. Der Schwerpunkt meiner Arbeit liegt in der Beratung von Fintech-Unternehmen, Software-Herstellern und Startups.</p>
       {/* Content goes here */}
+      <FaChevronDown className='text-2xl absolute bottom-20 left-1/2 left-1/2 transform -translate-x-1/2'/>
     </div>
   );
 };
